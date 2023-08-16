7322ec27fbb0635995a1c418ec2784fab06ca6d0
duykhanh-cnn <cnn22@connectpharm.vn>
2023-08-17 00:32:34 +0700

    publish swagger endpoint

--- trackingPlatform/Program.cs
+++ trackingPlatform/Program.cs
@@ -90,15 +90,9 @@ builder.Services.AddScoped<DangBaoCheRepositoryServices>();
 
 var app = builder.Build();
 
-
-
-
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
